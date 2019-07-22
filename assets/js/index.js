4c4e72af60203ea64ffcbca4a3f12af6d329f5d2
Joseph Schultz <joseph@acupajoe.io>
2019-07-21 21:28:33 -0500

    Restructured, dark theme, media-queries, mobile changes.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,32 @@
 $(function() {
   hljs.initHighlightingOnLoad();
+
+  $(".kg-gallery-container").prepend(
+    "<div id='kg-gallery-zoom' style='display:none;'><img src=''/><div id='kg-gallery-buttons'></div></div>"
+  );
+  $(".kg-gallery-container .kg-gallery-image img").each(function(e) {
+    var thissrc = $(this).attr("src");
+    var label = e + 1;
+    $("#kg-gallery-buttons").append(
+      '<span class="kg-gallery-btn" data-src="' +
+        thissrc +
+        '">' +
+        label +
+        "</span>"
+    );
+  });
+  $(".kg-gallery-btn").click(function(e) {
+    e.preventDefault();
+    $("#kg-gallery-zoom img").attr("src", $(this).data("src"));
+  });
+  $(".kg-gallery-image img").click(function(image) {
+    $("#kg-gallery-zoom img").attr("src", $(this).attr("src"));
+    $("#kg-gallery-zoom").fadeIn();
+    $(".floating-header").fadeOut();
+  });
+  $("#kg-gallery-zoom img").click(function(image) {
+    $("#kg-gallery-zoom img").attr("src", "");
+    $("#kg-gallery-zoom").fadeOut();
+    $(".floating-header").fadeIn();
+  });
 });
