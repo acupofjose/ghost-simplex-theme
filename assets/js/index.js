e4ef94a042983b46a0fd2e1d7e0f8c3968d0a355
Joseph Schultz <joseph@acupajoe.io>
2019-07-29 16:53:21 -0500

    Fixes #2

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,34 +24,45 @@ $(function() {
     });
 
   // Gallery
+  // Generate a hidden full screen container view
   $(".kg-gallery-container").prepend(
     "<div id='kg-gallery-zoom' style='display:none;'><img src=''/><div id='kg-gallery-buttons'></div></div>"
   );
 
+  // Navigation buttons
   $(".kg-gallery-container .kg-gallery-image img").each(function(e) {
-    var thissrc = $(this).attr("src");
-    var label = e + 1;
+    var src = $(this).attr("src");
+    var index = e + 1;
     $("#kg-gallery-buttons").append(
-      '<span class="kg-gallery-btn" data-src="' +
-        thissrc +
-        '">' +
-        label +
-        "</span>"
+      `<span class="kg-gallery-btn" data-src="${src}" data-index="${index}"></span>`
     );
   });
 
+  // On fullscreen navigation button clicked
   $(".kg-gallery-btn").click(function(e) {
     e.preventDefault();
     $("#kg-gallery-zoom img").attr("src", $(this).data("src"));
+    $(".kg-gallery-btn").removeClass("active");
+    $(this).addClass("active");
   });
+
+  // Handle showing full screen gallery
   $(".kg-gallery-image img").click(function(image) {
+    // Set active button on navigation
+    var index = $(this)
+      .parent()
+      .index();
+    $(".kg-gallery-btn").removeClass("active");
+    $(".kg-gallery-btn")
+      .eq(index)
+      .addClass("active");
+
     $("#kg-gallery-zoom img").attr("src", $(this).attr("src"));
     $("#kg-gallery-zoom").fadeIn();
-    $(".floating-header").fadeOut();
   });
+
   $("#kg-gallery-zoom img").click(function(image) {
     $("#kg-gallery-zoom img").attr("src", "");
     $("#kg-gallery-zoom").fadeOut();
-    $(".floating-header").fadeIn();
   });
 });
